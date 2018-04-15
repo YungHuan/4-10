d34e52eede8d29c181c2812c590819ae2d50147d
YONGHUAN <106021016@live.asia.edc.tw>
2018-04-15 22:56:17 +0800

    123

---
+++ shape.java
@@ -0,0 +1,19 @@
+public abstract class shape {
+    private double area,perimeter;
+    public shape(){
+
+    }
+    public  shape(double area1 , double perimeter1){
+        this();
+        setArea(area1);
+
+
+    }
+    public void setArea(double area1){
+        area=area1;
+    }
+    public void setPerimeter(double perimeter1){
+        perimeter=perimeter1;
+    }
+    public abstract void show();
+}
